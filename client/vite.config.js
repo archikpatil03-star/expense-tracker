1be19a90b90bd20d190bf6f98533519924364f2c
archikpatil03-star <archikpatil03@gmail.com>
2026-02-02 16:13:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['expense-tracker-reis.onrender.com'],   // 👈 ADD THIS LINE
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
