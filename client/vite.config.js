3c7e0cf02db2d11de95037f5f793c29479d7ea25
archikpatil03-star <archikpatil03@gmail.com>
2026-02-02 17:18:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    allowedHosts: [
+      'expense-tracker-front-32lc.onrender.com'
+    ]
+  }
 })
